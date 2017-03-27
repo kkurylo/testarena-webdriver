68fd79ea8094693f6f32ae3021c0ede06df44403
Kamila Kuryło <kamila888k@gmail.com>
2017-03-27 19:50:35 +0200

    Utworzenie ProjectPage

---
+++ src/main/java/chromedriver/ProjectsPage.java
@@ -0,0 +1,45 @@
+package chromedriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProjectsPage {
+
+    WebDriver driver;
+
+    @FindBy(css="a[href='http://demo.testarena.pl/administration/add_project'")
+    private WebElement addProjectButton;
+
+    @FindBy(id="name")
+    private WebElement nameOfProjectField;
+
+    @FindBy(id="prefix")
+    private WebElement prefixOfProjectField;
+
+    @FindBy(id="description")
+    private WebElement descriptionOfProjectField;
+
+    @FindBy(id="save")
+    private WebElement saveProjectButton;
+
+    public void clickAddProjectButton() {
+        addProjectButton.click();
+    }
+
+    public void setProjectName(String name) {
+        nameOfProjectField.sendKeys(name);
+    }
+
+    public void setPojectPrefix(String prefix) {
+        prefixOfProjectField.sendKeys(prefix);
+    }
+
+    public void setProjectDescription(String description) {
+        descriptionOfProjectField.sendKeys(description);
+    }
+
+    public void saveNewProject() {
+        saveProjectButton.click();
+    }
+}
